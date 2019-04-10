455f773f9be16468145c767f47f23e8de9002e38
mat <justmat@mykolab.com>
2019-04-10 18:11:59 -0400

    tabs v spaces :/

--- lib/tooloops.lua
+++ lib/tooloops.lua
@@ -4,39 +4,39 @@
 sc = {}
 
 function sc.init()
-    audio.level_cut(1.0)
-	audio.level_adc_cut(.5)
-	audio.level_eng_cut(0)
-    softcut.level_input_cut(1, 1, 1)
-    softcut.level_input_cut(2, 1, 0)
-    softcut.level_input_cut(1, 2, 0)
-    softcut.level_input_cut(2, 2, 1)
+  audio.level_cut(1.0)
+  audio.level_adc_cut(.5)
+  audio.level_eng_cut(0)
+  softcut.level_input_cut(1, 1, 1)
+  softcut.level_input_cut(2, 1, 0)
+  softcut.level_input_cut(1, 2, 0)
+  softcut.level_input_cut(2, 2, 1)
 
-	for i = 1, 2 do
-		softcut.enable(i, 1)
-		softcut.level(i, 1)
-		softcut.buffer(i, i)
+  for i = 1, 2 do
+    softcut.enable(i, 1)
+    softcut.level(i, 1)
+    softcut.buffer(i, i)
 
-		softcut.rate(i, 3)
-		softcut.play(i, 1)
+    softcut.rate(i, 3)
+    softcut.play(i, 1)
 
-		softcut.position(i, 1)
-		softcut.fade_time(i, 0.1)
+    softcut.position(i, 1)
+    softcut.fade_time(i, 0.1)
 
-		softcut.loop(i, 1)
-		softcut.loop_start(i, 1)
-		softcut.loop_end(i, 2)
+    softcut.loop(i, 1)
+    softcut.loop_start(i, 1)
+    softcut.loop_end(i, 2)
 
-		softcut.rec(i, 1)
-		softcut.rec_level(i, 1)
-		softcut.pre_level(i, .75)
+    softcut.rec(i, 1)
+    softcut.rec_level(i, 1)
+    softcut.pre_level(i, .75)
 
-		softcut.rate_slew_time(i, 0)
-		softcut.level_slew_time(1,0.25)
+    softcut.rate_slew_time(i, 0)
+    softcut.level_slew_time(1,0.25)
 
-		softcut.filter_dry(i, 1)
-	  softcut.filter_fc(i, 1200)
-	  softcut.filter_lp(i, 1)
+    softcut.filter_dry(i, 1)
+    softcut.filter_fc(i, 1200)
+    softcut.filter_lp(i, 1)
     softcut.filter_rq(i, 5)
 	end
 
@@ -49,22 +49,22 @@ function sc.init()
 
   params:add_separator()
 
-	for i = 1, 2 do
-	  -- tape speed controls
-		params:add_control(i .. "speed", i .. " speed", controlspec.new(-8, 8, "lin", 0.05, 8, ""))
-		params:set_action(i .. "speed", function(x) softcut.rate(i, x) end)
-		-- tape speed slew controls
-		params:add_control(i .. "speed_slew", i .. " speed slew", controlspec.new(0, 1, "lin", 0, 0.1, ""))
-		params:set_action(i .. "speed_slew", function(x) softcut.rate_slew_time(i, x) end)
-		-- tape length controls
-		params:add_control(i .. "tape_len", i .. " tape length", controlspec.new(.5, 16, "lin", 0, 16, "secs"))
-		params:set_action(i .. "tape_len", function(x) softcut.loop_end(i, x) end)
-		-- feedback controls
-		params:add_control(i .. "feedback", i .. " feedback", controlspec.new(0, 1, "lin", 0, .75, ""))
-		params:set_action(i .. "feedback", function(x) softcut.pre_level(i, x) end)
-		-- pan controls
-		params:add_control(i .. "pan", i .. " pan", controlspec.new(0, 1, "lin", 0, i == 1 and 1 or 0, ""))
-		params:set_action(i .. "pan", function(x) softcut.pan(i, x) end)
+  for i = 1, 2 do
+    -- tape speed controls
+    params:add_control(i .. "speed", i .. " speed", controlspec.new(-8, 8, "lin", 0.05, 8, ""))
+    params:set_action(i .. "speed", function(x) softcut.rate(i, x) end)
+    -- tape speed slew controls
+    params:add_control(i .. "speed_slew", i .. " speed slew", controlspec.new(0, 1, "lin", 0, 0.1, ""))
+    params:set_action(i .. "speed_slew", function(x) softcut.rate_slew_time(i, x) end)
+    -- tape length controls
+    params:add_control(i .. "tape_len", i .. " tape length", controlspec.new(.5, 16, "lin", 0, 16, "secs"))
+    params:set_action(i .. "tape_len", function(x) softcut.loop_end(i, x) end)
+    -- feedback controls
+    params:add_control(i .. "feedback", i .. " feedback", controlspec.new(0, 1, "lin", 0, .75, ""))
+    params:set_action(i .. "feedback", function(x) softcut.pre_level(i, x) end)
+    -- pan controls
+    params:add_control(i .. "pan", i .. " pan", controlspec.new(0, 1, "lin", 0, i == 1 and 1 or 0, ""))
+    params:set_action(i .. "pan", function(x) softcut.pan(i, x) end)
     -- filter controls
     params:add_control(i .. "cutoff", i .. " cutoff", controlspec.new(10, 12000, 'exp', 1, 12000, "Hz"))
     params:set_action(i .. "cutoff", function(x) softcut.filter_fc(i, x) end)
@@ -75,8 +75,8 @@ function sc.init()
     params:add_control(i .. "filter_dry", i .. " filter dry", controlspec.new(0, 1, 'lin', 0, 0, ""))
     params:set_action(i .."filter_dry", function(x) softcut.filter_dry(i, x) end)
 
-		params:add_separator()
-	end
+    params:add_separator()
+  end
 end
 
 

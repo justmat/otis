5ae41becee485653aa3282d3e42916af872d93cb
mat <justmat@mykolab.com>
2019-11-14 15:20:51 -0500

    remove unused code
    how'd that get back in here :/

--- lib/hnds.lua
+++ lib/hnds.lua
@@ -54,12 +54,6 @@ function lfo.scale(old_value, old_min, old_max, new_min, new_max)
 end
 
 
-local function set_lfo_min(i, v)
-  lfo[i].min_val = util.clamp(v, 0.0, params:get(i .. "lfo_max"))
-  params:set(i .."lfo_min", lfo[i].min_val)
-end
-
-
 local function make_sine(n)
   return 1 * math.sin(((tau / 100) * (lfo[n].counter)) - (tau / (lfo[n].freq)))
 end

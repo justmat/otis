bd0d5e696586b1f0e3ce8f27d9cfdad750290f36
justmat <justmat@mykolab.com>
2020-11-02 19:54:01 +0700

    rec_offset adjustment

--- lib/tlps.lua
+++ lib/tlps.lua
@@ -76,7 +76,7 @@ function sc.init()
     softcut.loop_end(i, 60)
 
     softcut.rec(i, 1)
-    softcut.rec_offset(i, -0.06)
+    softcut.rec_offset(i, -0.00016)
     softcut.rec_level(i, 1)
     softcut.pre_level(i, .75)
 
